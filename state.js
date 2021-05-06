9724db3b4636983f25aff065738663081fdda700
Sanshain <digital-mag@ya.ru>
2021-05-06 23:36:46 +0300

    init

---
+++ state.js
@@ -0,0 +1,42 @@
+
+// //@ts-check
+
+import { useState } from 'react/hooks'
+
+
+let states = {};
+
+export function setState(key, value){
+
+	for (const hook of states[key].hooks) {
+		
+		// console.log(hook);
+		hook(value)
+	}
+}
+
+export function useStore(key, init_value){
+
+	const [value, setValue] = useState(init_value !== undefined ? init_value : (states[key] || {}).initState)
+	if (key in states){
+		
+		!~states[key].hooks.indexOf(setValue) && states[key].hooks.push(setValue);
+	}
+	else states[key] =  {
+		hooks: [setValue],
+		initState: init_value
+	}
+	
+	return [value, setState.bind(null, key)]
+}
+
+export function initStore(init_dict){
+
+	for (const key in init_dict) {
+		states[key] = {
+			hooks: [],
+			initState: init_dict[key]			
+		}
+	}
+}
+
